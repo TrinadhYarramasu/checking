6706a5006ffc759c6d12f0ef17a185f343dee7ba
Komirelly <Komirelly@kaykpc>
2020-01-25 15:23:16 +0530

    first commit

---
+++ checking/src/test/java/com/kmlpay/CheckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmlpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
